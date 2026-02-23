b4674d9347a41ba795cab7b32ca8042d04b4bfe5
threelyK <scmichael23@yahoo.com>
2026-02-23 15:12:03 +0000

    Added Radio to prep for STT

---
+++ Assets/Scripts/Player/RadioManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class RadioManager : MonoBehaviour
+{
+
+    private bool _transmissionPending;
+
+    // TODO:
+    // - Display pending transmission status
+    // - Notify modules that handle STT
+}
