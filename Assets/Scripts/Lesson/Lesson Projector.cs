1ced6480ee2d3b80663082079f06302e255f128d
threelyK <scmichael23@yahoo.com>
2026-04-28 01:13:09 +0100

    Spinning on Z axis

--- Assets/Scripts/Lesson/Lesson Projector.cs
+++ Assets/Scripts/Lesson/Lesson Projector.cs
@@ -2,15 +2,7 @@ using UnityEngine;
 
 public class LessonProjector : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // current lesson
+    public string lessonID;
+    
 }
---
+++ Assets/Scripts/Lesson/SpinZ.cs
@@ -0,0 +1,23 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class SpinZ : MonoBehaviour
+{
+    [SerializeField] private float _interval = 5f;
+    [SerializeField] private bool _onZaxis = true;
+    
+    private void Start()
+    {
+        var rot = _onZaxis ? new Vector3(0, 0, 360) : new Vector3(360, 0, 0);
+        
+        gameObject.transform.DOLocalRotate(rot, _interval, RotateMode.LocalAxisAdd)
+            .SetEase(Ease.Linear).SetLoops(-1, LoopType.Incremental);
+
+        foreach (Transform child in gameObject.transform)
+        {
+            child.DOLocalRotate(rot * -1, _interval, RotateMode.LocalAxisAdd)
+                .SetEase(Ease.Linear)
+                .SetLoops(-1, LoopType.Incremental);
+        }
+    }
+}
